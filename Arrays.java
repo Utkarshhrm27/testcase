b68008e31b33847e2f6bbb6d9ba4ef8bf67be2a9
Utkarshhrm27 <utkarshrmishra27@gmail.com>
2025-10-28 20:55:13 +0530

    Utkarshhrm27

---
+++ Arrays.java
@@ -0,0 +1,33 @@
+public class Arrays {
+    public static void main(String[] args) {
+        // Array is collection of similar type of data
+        // There are three ways to declare an array the first way is written below:
+        /*
+         * int[] marks ;
+         * marks = new int[5]; // syntax1
+         * marks[0] = 100;
+         * marks[1] = 100;
+         * marks[2] = 100;
+         * marks[3] = 100;
+         * marks[4] = 100;
+         * System.out.println(marks[4]);
+         */
+        
+        /*2nd way to print an array
+         int[] num = new int[5];
+         3rd way to print an array*/
+
+        /*
+         int[] arr = { 100, 200, 300, 400, 500, 600, 700 };
+         
+         for (int i = 5; i < arr.length; i++)
+         To return index of and an array this for loops traverses the
+         whole array checks the case is true or not and then returns the exact
+         index of array .
+         
+         System.out.println(i);
+         */ // RETURNS THE INDEX OF AN ARRAY
+
+    }
+
+}
---
+++ Arrays2.java
@@ -0,0 +1,49 @@
+public class Arrays2 {
+    public static void main(String[] args) {
+        /* 
+        int[] arr ={400,37,7,4};
+        System.out.println(arr.length);// "(Name Of Array).length" Will return
+        "LENGTH" of an array.*/
+         
+        /* 
+        To make a floating point array
+        float[] arrr= {47.5f,37.5f,17.5f,7.5f};
+        System.out.println(arrr.length); */
+
+        /* 
+        TO MAKE A STRING ARRAY
+        System.out.println("Printing using Naive Method: ");
+
+        String [] name ={"utkarsh","aditya","sankalp","vinay","milin","vrishketu","sarfarz"};
+        System.out.println(name.length);
+        System.out.println(name[1]); 
+        System.out.println(name[2]);
+        System.out.println(name[3]);
+        System.out.println(name[4]);
+        System.out.println(name[5]);
+        System.out.println(name[6]);*/
+
+        //Displaying array using for loop
+        /*
+        System.out.println("Printing using for loop:")
+        int[]arr={7,4,17,14,37};
+        for(int i=0;i<=arr.length;i++){
+        System.out.println(arr[i]);}*/
+
+        //Printing array elements using for each loop:BEST WAY IF NO CONDITIONS
+        /*
+        System.out.println("PRINTING THE ARRAY USING FOR EACH LOOP WAY: ");
+        int[]arr={7,4,17,14,37};
+        for( int elements: arr){
+        System.out.println(elements);}*/
+
+        //QUIZ : Create an Array to build elements in an array in reverse order.
+        /*
+        int[]arr={7,4,17,14,37};
+        for( int i=arr.length-1;i>=0;i--){
+        System.out.println(arr[i]);}*/
+
+        
+       
+    }
+}
