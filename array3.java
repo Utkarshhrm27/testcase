a08814c929aefa24e2713e1523ecf9e2b802371a
Utkarshhrm27 <utkarshrmishra27@gmail.com>
2025-10-29 22:10:34 +0530

    add new code

--- array3.java
+++ array3.java
@@ -1,3 +1,12 @@
-public  {
+public class array3 {
+    public static void main(String[] args) {
+        int[] arr = { 100, 200, 300, 400, 500, 600, 700 };
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 500) {
+                System.out.println(i);
+            }
+        }
+    }
     
 }
